180af5566ef7067ce4866d17568deefcb8020fb7
zhy1stgg <823402333@qq.com>
2015-09-19 20:46:47 +0800

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el: '#plugins-list',
     data: {
-        plugins: null,
+        plugins: null, 
         search: null,
         msg: null,
         pageSize: 10,
@@ -13,11 +13,10 @@ var app = new Vue({
         }, {
             value: 10,
             text: '10'
-        },{
-        	value: 20,
-        	text: '20'
+        }, {
+            value: 20,
+            text: '20'
         }]
-
     },
     methods: {
         changePage: function(page) {
@@ -33,28 +32,30 @@ var app = new Vue({
 
 Vue.filter('searchFilter', function(value, search) {
 
-
     if (value.length < app.pageSize * app.pageNum) {
         app.pageNum = Math.ceil(value.length / app.pageSize);
     }
 
     var newValue = [];
+    // 确定当前页内容
     _.each(value, function(v, i) {
         if (i >= app.pageSize * (app.pageNum - 1) && i <= app.pageSize * (app.pageNum)) {
             newValue.push(v);
         }
     });
 
-
+    // 计算总椰树
     app.totalPage = Math.ceil(app.plugins.length / app.pageSize);
 
+
+    // 如果不存在搜索内容，直接返回结果
     if (search === null || search === "") {
         app.msg = null;
-
         return newValue;
+
     } else {
 
-        // var newValue = [];
+    	// 根据搜索内容过滤
         _.each(value, function(v, i) {
             if (v.name.indexOf(search) !== -1 || v.description.indexOf(search) !== -1 || v.author.name.indexOf(search) !== -1) {
                 newValue.push(v);
@@ -63,13 +64,11 @@ Vue.filter('searchFilter', function(value, search) {
 
         if (newValue.length > 0) {
             app.msg = '共搜索到 ' + newValue.length + ' 条记录';
-            // app.totalPage = Math.ceil(newValue.length /app.pageSize);
             // Todo 搜索结果进行分页处理
             return newValue;
         } else {
             app.msg = '无搜索结果，请重新输入';
             app.totalPage = 0;
-
             return [];
         }
     }
