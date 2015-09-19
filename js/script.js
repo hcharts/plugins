1886225ac8a2d33a022f07208b16165938c1bd81
zhy1stgg <823402333@qq.com>
2015-09-19 20:36:09 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+var app = new Vue({
+    el: '#plugins-list',
+    data: {
+        plugins: null,
+        search: null,
+        msg: null,
+        pageSize: 10,
+        pageNum: 1,
+        totalPage: 0,
+        pageSizeOptions: [{
+            value: 5,
+            text: '5'
+        }, {
+            value: 10,
+            text: '10'
+        },{
+        	value: 20,
+        	text: '20'
+        }]
+
+    },
+    methods: {
+        changePage: function(page) {
+            if (page > this.totalPage) {
+                return;
+            }
+            this.pageNum = page;
+            jQuery('body').scrollTop(0);
+        }
+    }
+});
+
+
+Vue.filter('searchFilter', function(value, search) {
+
+
+    if (value.length < app.pageSize * app.pageNum) {
+        app.pageNum = Math.ceil(value.length / app.pageSize);
+    }
+
+    var newValue = [];
+    _.each(value, function(v, i) {
+        if (i >= app.pageSize * (app.pageNum - 1) && i <= app.pageSize * (app.pageNum)) {
+            newValue.push(v);
+        }
+    });
+
+
+    app.totalPage = Math.ceil(app.plugins.length / app.pageSize);
+
+    if (search === null || search === "") {
+        app.msg = null;
+
+        return newValue;
+    } else {
+
+        // var newValue = [];
+        _.each(value, function(v, i) {
+            if (v.name.indexOf(search) !== -1 || v.description.indexOf(search) !== -1 || v.author.name.indexOf(search) !== -1) {
+                newValue.push(v);
+            }
+        });
+
+        if (newValue.length > 0) {
+            app.msg = '共搜索到 ' + newValue.length + ' 条记录';
+            // app.totalPage = Math.ceil(newValue.length /app.pageSize);
+            // Todo 搜索结果进行分页处理
+            return newValue;
+        } else {
+            app.msg = '无搜索结果，请重新输入';
+            app.totalPage = 0;
+
+            return [];
+        }
+    }
+});
+
+
+// Get the data and set to app.plugins
+$.getJSON('manifest.json', function(data) {
+    app.plugins = data;
+});
