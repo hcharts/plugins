887a337fecd9f2e6822f6beed1982625821c6f6b
zhy1stgg <823402333@qq.com>
2015-09-19 23:26:31 +0800

    fixed search paging

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 var app = new Vue({
     el: '#plugins-list',
     data: {
-        plugins: null, 
+        plugins: null,
         search: null,
         msg: null,
         pageSize: 10,
         pageNum: 1,
         totalPage: 0,
         pageSizeOptions: [{
+            value: 2,
+            text: '2'
+        }, {
             value: 5,
             text: '5'
         }, {
@@ -32,46 +35,49 @@ var app = new Vue({
 
 Vue.filter('searchFilter', function(value, search) {
 
-    if (value.length < app.pageSize * app.pageNum) {
-        app.pageNum = Math.ceil(value.length / app.pageSize);
-    }
-
     var newValue = [];
-    // 确定当前页内容
-    _.each(value, function(v, i) {
-        if (i >= app.pageSize * (app.pageNum - 1) && i <= app.pageSize * (app.pageNum)) {
-            newValue.push(v);
-        }
-    });
+    var totalElements = app.plugins.length;
 
-    // 计算总椰树
-    app.totalPage = Math.ceil(app.plugins.length / app.pageSize);
-
-
-    // 如果不存在搜索内容，直接返回结果
-    if (search === null || search === "") {
+    if (search === null | search === "") {
         app.msg = null;
-        return newValue;
-
+        newValue = value;
     } else {
-
-    	// 根据搜索内容过滤
+        // 根据搜索内容过滤
         _.each(value, function(v, i) {
             if (v.name.indexOf(search) !== -1 || v.description.indexOf(search) !== -1 || v.author.name.indexOf(search) !== -1) {
                 newValue.push(v);
             }
         });
 
-        if (newValue.length > 0) {
-            app.msg = '共搜索到 ' + newValue.length + ' 条记录';
-            // Todo 搜索结果进行分页处理
-            return newValue;
+        totalElements = newValue.length;
+
+        if (totalElements > 0) {
+            app.msg = '共搜索到 ' + totalElements + ' 条记录';
         } else {
             app.msg = '无搜索结果，请重新输入';
             app.totalPage = 0;
             return [];
         }
     }
+
+    // 确定当前页内容
+    var pageVlue = [];
+    _.each(newValue, function(v, i) {
+        if (i >= app.pageSize * (app.pageNum - 1) && i < app.pageSize * (app.pageNum)) {
+            pageVlue.push(v);
+        }
+    });
+
+    // 计算总页数
+    app.totalPage = Math.ceil(totalElements / app.pageSize);
+
+    // 如果页码超过总页数，则调到最后一页
+    if (totalElements < app.pageSize * app.pageNum) {
+        app.pageNum = app.totalPage;
+    }
+
+    return pageVlue;
+
 });
 
 
